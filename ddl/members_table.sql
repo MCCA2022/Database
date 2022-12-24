679ddd74f9dc7510d34b5332aace9d8e559b2f16
sickdatascientist <sick.datascientist@gmail.com>
2022-12-24 03:48:21 +0000

    Added DDL for table membership

--- ddl/members_table.sql
+++ ddl/members_table.sql
@@ -1,12 +1,26 @@
 -- !preview conn=mssql$connection
 
 USE mcca
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='members' and xtype='U')
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='membership' and xtype='U')
   BEGIN
-    CREATE TABLE members (
-      id INT PRIMARY KEY IDENTITY (1, 1),
-      first_name VARCHAR,
-      middle_name VARCHAR,
-      last_name VARCHAR
+    CREATE TABLE membership (
+      id INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+      email VARCHAR(100) NOT NULL,
+      first_name VARCHAR(100) NOT NULL,
+      middle_name VARCHAR(100) NOT NULL,
+      last_name VARCHAR(100) NOT NULL,
+      birth_date VARCHAR(100) NOT NULL,
+      barangay VARCHAR(100) NOT NULL,
+      city_municipality VARCHAR(100) NOT NULL,
+      province VARCHAR(100) NOT NULL,
+      region VARCHAR(100) NOT NULL,
+      contact_number VARCHAR(100) NOT NULL,
+      level CHARACTER(100) NOT NULL,
+      affiliations VARCHAR(100) NOT NULL,
+      affiliation_id VARCHAR(100) NOT NULL,
+      lichess_username VARCHAR(100) NOT NULL,
+      reason_and_goals_with_chess VARCHAR,
+      RA10173 CHARACTER(100) NOT NULL,
+      application_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
     )
   END
